fac45ec10e94863d3512917d1915433e6a4653d9
yuetianluo <luoyuetian123@ruc.edu.cn>
2016-08-09 22:37:58 -0700

    add more time

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,3 @@
 module.exports.orm = {
-  _hookTimeout: 60000 // I used 60 seconds as my new timeout
+  _hookTimeout: 600000 // I used 60 seconds as my new timeout
 };
--- config/pubsub.js
+++ config/pubsub.js
@@ -1,3 +1,3 @@
 module.exports.pubsub = {
-  _hookTimeout: 60000 // I used 60 seconds as my new timeout
+  _hookTimeout: 600000 // I used 60 seconds as my new timeout
 };//!!!!!!!!!!!!!!because the size of data is so large, so you need more time
